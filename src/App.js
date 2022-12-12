596fc31d58c06b43dc1045595b92b7339f75c715
SmeyerGit <nilsschmeierprio@gmail.com>
2022-12-12 12:14:33 +0100

    logo fix

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import React from 'react';
 function App() {
   const value = 'World';
   return <div>Hellosss {value}</div>;
-  <img src="C:\Users\nis\react-azure-app\public\dS_logo_rgb_1068_180.png" alt="Logo"></img>
+  <img src="public\dS_logo_rgb_1068_180.png" alt="Logo"></img>
 }
-<img src="C:\Users\nis\react-azure-app\public\dS_logo_rgb_1068_180.png" alt="Logo"></img>
+<img src="public\dS_logo_rgb_1068_180.png" alt="Logo"></img>
+
 export default App;
