a49d996e5728442b34133369a7baaa7d5de2c154
SmeyerGit <nilsschmeierprio@gmail.com>
2022-12-12 12:58:34 +0100

    logo

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
 function App() {
-  return <div>dotSource </div>;
-
+  return <div>dotSource <img src='public\dS_logo_rgb_1068_180.png' alt='logo'></img></div>;
+ 
 }
 
 
