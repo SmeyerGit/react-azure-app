1e1b6b25628950f15f238525dd083d257e941519
SmeyerGit <nilsschmeierprio@gmail.com>
2022-12-12 12:51:21 +0100

    rss

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 function App() {
   const value = 'World';
   return <div>dotSource {value}</div>;
-  <img src="public\dS_logo_rgb_1068_180.png" alt="Logo"></img>;
+  
   <p>seite in Bearbeitung</p>;
 }
 
