93ce01a7617734d154d39f88aeadd3ef756b68e5
SmeyerGit <nilsschmeierprio@gmail.com>
2022-12-12 12:30:34 +0100

    text

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import React from 'react';
 function App() {
   const value = 'World';
   return <div>Hellosss {value}</div>;
-  <img src="public\dS_logo_rgb_1068_180.png" alt="Logo"></img>
+  <img src="public\dS_logo_rgb_1068_180.png" alt="Logo"></img>;
+  <p>seite in Bearbeitung</p>;
 }
-<img src="public\dS_logo_rgb_1068_180.png" alt="Logo"></img>
+
 
 export default App;
